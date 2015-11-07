cef23aa798532c8b8d64348a26a353da295e033e
lelysi <denis.makovey@gmail.com>
2015-11-07 21:08:33 +0300

    create test class PlayerTest

---
+++ tst/io/hexlet/xo/model/PlayerTest.java
@@ -0,0 +1,35 @@
+package io.hexlet.xo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lelysi on 07.11.2015.
+ */
+public class PlayerTest {
+
+    @Test
+    public void testGetName() throws Exception {
+        final String inputValue = "John";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void testGetFigure() throws Exception {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
+    }
+}
