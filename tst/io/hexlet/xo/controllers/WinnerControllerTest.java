60974d53fccca853e5fcae72ea9d949f1ed12adf
lelysi <denis.makovey@gmail.com>
2015-11-08 13:08:53 +0300

    created test class WinnerControllerTest

---
+++ tst/io/hexlet/xo/controllers/WinnerControllerTest.java
@@ -0,0 +1,67 @@
+package io.hexlet.xo.controllers;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lelysi on 08.11.2015.
+ */
+public class WinnerControllerTest {
+
+    @Test
+    public void testGetWinnerWhenWinnerRow() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+
+        for (int i = 0; i < 3; i++) {
+            final Field field = new Field(3);
+            field.setFigure(new Point(i, 0), Figure.X);
+            field.setFigure(new Point(i, 1), Figure.X);
+            field.setFigure(new Point(i, 2), Figure.X);
+            assertEquals(Figure.X, winnerController.getWinner(field));
+        }
+    }
+
+    @Test
+    public void testGetWinnerWhenNoRow() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+
+        for (int i = 0; i < 3; i++) {
+            final Field field = new Field(3);
+            field.setFigure(new Point(i, 0), Figure.X);
+            field.setFigure(new Point(i, 1), Figure.X);
+            field.setFigure(new Point(i, 2), Figure.O);
+            assertNull(winnerController.getWinner(field));
+        }
+    }
+
+    @Test
+    public void testGetWinnerWhenWinnerColumn() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+
+        for (int i = 0; i < 3; i++) {
+            final Field field = new Field(3);
+            field.setFigure(new Point(0, i), Figure.X);
+            field.setFigure(new Point(1, i), Figure.X);
+            field.setFigure(new Point(2, i), Figure.X);
+            assertEquals(Figure.X, winnerController.getWinner(field));
+        }
+    }
+
+    @Test
+    public void testGetWinnerWhenNoWinnerColumn() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+
+        for (int i = 0; i < 3; i++) {
+            final Field field = new Field(3);
+            field.setFigure(new Point(0, i), Figure.X);
+            field.setFigure(new Point(1, i), Figure.X);
+            field.setFigure(new Point(2, i), Figure.O);
+            assertNull(winnerController.getWinner(field));
+        }
+    }
+}
